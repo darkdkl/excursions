b4c2e7ad5028977642a81bc98ee78d48311dc8c2
Dark_Dmake <dark.dmake@gmail.com>
2020-08-15 18:36:11 +0300

    remove null=True

--- places/models.py
+++ places/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=150, verbose_name="Заголовок")
-    short_description = models.TextField(blank=True,null=True,
+    short_description = models.TextField(blank=True,
                                          verbose_name="Краткое описание")
-    long_description= models.TextField(blank=True,null=True,
+    long_description= models.TextField(blank=True,
                                        verbose_name="Полное описание")
     lat = models.FloatField(verbose_name="Широта")
     lng = models.FloatField(verbose_name="Долгота")
